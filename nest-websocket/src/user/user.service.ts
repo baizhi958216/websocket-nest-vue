f1cb7fe9059fe91b6dd019b2e8049c281833d842
baizhi958216 <1475289190@qq.com>
2023-10-13 17:19:25 +0800

    prisma存储用户信息
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- nest-websocket/src/user/user.service.ts
+++ nest-websocket/src/user/user.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { db } from 'lib/db';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  async create(createUserDto: CreateUserDto) {
+    const user = await db.user.create({
+      data: createUserDto,
+    });
+    return user;
   }
 }
